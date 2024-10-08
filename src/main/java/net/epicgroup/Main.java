e4738cafdcbc3ac17c266d40cb370e9a08b7f3eb
Mikits4 <mikaoberholzer0@gmail.com>
2024-10-08 19:34:57 +0200

    Innitial commit

---
+++ src/main/java/net/epicgroup/Main.java
@@ -0,0 +1,40 @@
+package net.epicgroup;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Phonebook Menu:");
+        System.out.println("1. Insert Contact");
+        System.out.println("2. Search Contact");
+        System.out.println("3. Display All Contacts");
+        System.out.println("4. Delete Contact");
+        System.out.println("5. Update Contact");
+        System.out.println("6. Sort Contacts");
+        System.out.print("Enter your choice: ");
+
+    }
+
+    private static void insertContact() {
+
+    }
+
+    private static void searchContact() {
+
+    }
+
+    private static void displayContacts() {
+
+    }
+
+    private static void deleteContact() {
+
+    }
+
+    private static void updateContact() {
+
+    }
+
+    private static void sortContacts() {
+
+    }
+}
